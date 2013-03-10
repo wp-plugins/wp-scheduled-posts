1109b6f1e422c42d5a863b7f22dace377b8af390
Asif2BD <Asif2BD@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-10 07:11:24 +0000

    Initial commit 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-scheduled-posts/trunk@678998 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/wpscp-options.php
@@ -0,0 +1,122 @@
+<?php 
+
+function wpscp_get_options()
+{
+	$options=array(
+			'show_dashboard_widget'=>1, 
+			'show_in_front_end_adminbar'=>1, 
+			'show_in_adminbar'=>1,
+			'allow_user_role'=>array('administrator'),
+			'allow_post_types'=>array('post')  
+	
+	);
+return get_option('wpscp_options',$options);
+}
+
+function wpscp_permit_user()
+{
+global $current_user;
+$wpscp_options=wpscp_get_options();
+
+if(!is_array($current_user->roles)) return false;
+if(!is_array($wpscp_options['allow_user_role']))$wpscp_options['allow_user_role']=array('administrator');
+
+	foreach($current_user->roles as $ur)
+	{
+		if(in_array($ur, $wpscp_options['allow_user_role'])) {return true; break;}
+	}
+
+return false;
+}
+
+function wpscp_dropdown_roles( $selected = array() ) { #modified function from function wp_dropdown_roles( $selected = false ) in wp-admin/include/template.php
+	$p = '';
+	$r = '';
+	$editable_roles = get_editable_roles();
+	foreach ( $editable_roles as $role => $details ) {
+		$name = translate_user_role($details['name'] ); 
+		if ( in_array(esc_attr($role),$selected) ) // preselect specified role
+			{
+			$p .= "\n\t<option selected='selected' value='" . esc_attr($role) . "'>$name</option>";
+			}
+		else
+			$r .= "\n\t<option value='" . esc_attr($role) . "'>$name</option>";
+	}
+	echo $p . $r;
+
+}
+
+
+function wpscp_options_page()
+{
+	global $wpdb;
+	$wpscp_options=wpscp_get_options();
+	
+	if(isset($_POST['save_options']))
+	{
+		$options=array(
+				'show_dashboard_widget'=>intval($_POST['show_dashboard_widget']), 
+				'show_in_front_end_adminbar'=>intval($_POST['show_in_front_end_adminbar']), 
+				'show_in_adminbar'=>intval($_POST['show_in_adminbar']),
+				'allow_user_role'=>$_POST['allow_user_role'],
+				'allow_post_types'=>$_POST['allow_post_types']  
+		
+		);	
+	update_option('wpscp_options',$options);
+	$wpscp_options=$options;
+	}#end if(isset($_POST['save_options']))
+	
+	
+	echo "<div style=\"width: 1010px; padding-left: 10px;\" class=\"wrap\">";
+		echo "<div style=\"width: 800px; float:left;\">";
+		echo "<h2>WP Scheduled Posts Options</h2>";
+		global $current_user;
+		?>
+			<form action="" method="post">
+            <table class="form-table">
+            <tr><td width="280" align="right">Show Scheduled Posts in Dashboard Widget</td><td><input type="checkbox" name="show_dashboard_widget" value="1" <?php echo ($wpscp_options['show_dashboard_widget'])?' checked="checked"': '';?> /></td></tr>
+            <tr><td align="right">Show Scheduled Posts in Site Front-end Admin Bar</td><td><input type="checkbox" name="show_in_front_end_adminbar" value="1" <?php echo ($wpscp_options['show_in_front_end_adminbar'])?' checked="checked"': '';?>/></td></tr>
+            <tr><td align="right">Show Scheduled Posts in Admin Bar</td><td><input type="checkbox" name="show_in_adminbar" value="1" <?php echo ($wpscp_options['show_in_adminbar'])?' checked="checked"': '';?>/></td></tr>
+
+			<tr>
+            <td scope="row" style="text-align:right; vertical-align:top;">Show Post Types: </td>
+            <td>
+            <select name="allow_post_types[]" MULTIPLE style="height:70px;width:300px;">
+			<?php
+			$typeswehave = array('post,revision'); //oneTarek
+			$post_types=get_post_types('','names'); 
+			$rempost = array('attachment','revision','nav_menu_item');
+			$post_types = array_diff($post_types,$rempost);
+			foreach ($post_types as $post_type ) {
+				echo "<option ";
+				
+				if(in_array($post_type,$wpscp_options['allow_post_types'])) echo "selected ";
+				echo 'value="'.$post_type.'">'.$post_type.'</option>';
+			}
+			
+			?>
+			</select>
+            </td>
+            </tr>
+            
+            <tr valign="top">
+            <td scope="row" align="right"><label for="allow_user_role"><?php echo 'Allow users' ; ?></label></td>
+            <td>
+            <select name="allow_user_role[]" id="allow_user_role" multiple="multiple"  style="height:100px;width:300px;" ><?php  wpscp_dropdown_roles( $wpscp_options['allow_user_role'] ); ?></select>
+            </td>
+            </tr>  
+                        
+            <tr><td><input type="submit" name="save_options" value="Save Options" class='button-primary'/></td><td>&nbsp;</td></tr>
+            </table>
+            </form>
+<?php
+	
+		echo "</div>";
+	
+		include_once(WPSCP_PLUGIN_PATH."includes/wpscp-sidebar.php");
+		echo '<div style="clear:both"></div>';
+	echo "</div>";
+
+}
+
+?>
---
+++ wp-scheduled-posts.php
@@ -0,0 +1,120 @@
+
+<?php
+/*
+ * Plugin Name: WP Scheduled Posts
+ * Plugin URI: http://wpdeveloper.net
+ * Description: A complete solution for WordPress Scheduled Post. Get an admin Bar & Dashboard Widget showing all your scheduled post.
+ * Version: 1.0.0
+ * Author: WPDeveloper.net
+ * Author URI: http://wpdeveloper.net
+ * License: GPL2+
+ * Text Domain: wp-scheduled-posts
+ * Min WP Version: 2.5.0
+ * Max WP Version: 3.5.2
+ */
+
+
+define("WPSCP_PLUGIN_SLUG",'wp-scheduled-posts');
+define("WPSCP_PLUGIN_URL",plugins_url("",__FILE__ ));#without trailing slash (/)
+define("WPSCP_PLUGIN_PATH",plugin_dir_path(__FILE__)); #with trailing slash (/)
+
+include_once('includes/wpscp-options.php');
+include_once('includes/wpdev-dashboard-widget.php');
+
+function add_wpscp_menu_pages()
+
+{
+add_options_page( "WP Scheduled Posts", "WP Scheduled Posts" ,'manage_options', WPSCP_PLUGIN_SLUG, 'wpscp_options_page');
+}
+
+add_action('admin_menu', 'add_wpscp_menu_pages'); 
+
+	
+	function wp_scheduled_post_widget_function() {
+		global $wpdb;
+		$wpscp_options=wpscp_get_options();
+		$post_types=implode("', '",$wpscp_options['allow_post_types']); $post_types="'".$post_types."'";
+		$result=$wpdb->get_results("select * from ".$wpdb->prefix."posts where post_status = 'future' AND post_type IN(".$post_types.") ORDER BY post_date ASC ");
+
+		echo '<table class="widefat">';
+		foreach($result as $scpost)
+		{
+		echo '<tr><td><a href="'.get_edit_post_link($scpost->ID).'">'.$scpost->post_title.'</a></td><td>'.get_date_from_gmt($scpost->post_date_gmt, $format = 'Y-m-d H:i:s').'</td><td>'.get_the_author_meta( 'user_login', $scpost->post_author ).'</td></tr>';
+		
+		}
+		echo "</table>";
+		
+	} # END OF wp_scheduled_post_widget_function()
+
+// Create the function use in the action hook
+
+function wp_scp_add_dashboard_widgets()
+	{
+	global $current_user;
+	$wpscp_options=wpscp_get_options();
+	if($wpscp_options['show_dashboard_widget'])
+		{
+			if(wpscp_permit_user())
+			{
+			wp_add_dashboard_widget('wp_scp_dashboard_widget', 'WP Scheduled Posts', 'wp_scheduled_post_widget_function');	
+			}
+		}
+	} 
+// Hook into the 'wp_dashboard_setup' action to register our other functions
+add_action('wp_dashboard_setup', 'wp_scp_add_dashboard_widgets');
+
+#-----------ADMINBAR---------------------
+add_action( 'admin_bar_menu', 'wp_scheduled_post_menu', 1000 );
+
+
+  function wp_scheduled_post_menu() {
+  	  global $wp_admin_bar;
+	  $wpscp_options=wpscp_get_options();
+	  if($wpscp_options['show_in_adminbar'] || $wpscp_options['show_in_front_end_adminbar'])
+	  {
+		  if(is_admin() && !$wpscp_options['show_in_adminbar']) return;
+		  if(!is_admin() && !$wpscp_options['show_in_front_end_adminbar']) return;
+		  
+		  if(wpscp_permit_user())
+		  {
+				
+				
+				global $wpdb;
+				$post_types=implode("', '",$wpscp_options['allow_post_types']); $post_types="'".$post_types."'";
+				$result=$wpdb->get_results("select * from ".$wpdb->prefix."posts where post_status = 'future' AND post_type IN(".$post_types.") ORDER BY post_date ASC ");
+				$totalPost=0;
+				if(is_array($result)){$totalPost=count($result);}
+				$wp_admin_bar->add_menu( array( 'id' => 'wpscp', 'title' =>'Scheduled Posts ('.$totalPost.')') );
+				
+				if(is_array($result))
+				{
+					foreach($result as $scpost)
+					{
+					$wp_admin_bar->add_menu( array( 'parent' => 'wpscp' , 'title' =>substr($scpost->post_title, 0,45)."...&nbsp; - &nbsp By: ".get_the_author_meta( 'user_nicename', $scpost->post_author )."&nbsp For ".get_date_from_gmt($scpost->post_date_gmt, $format = 'h:i:a') , 'href' =>get_edit_post_link($scpost->ID),'meta'=>array('title'=>$scpost->post_title) ) );
+					}
+				}
+		  }
+	  }
+  }
+
+#-----------------------------SHOWING scheduled POSTS ON HOMEPAGE--------------------------------------
+function wp_scheduled_posts()
+{
+	global $wpdb;
+		$wpscp_options=wpscp_get_options();
+		$post_types=implode("', '",$wpscp_options['allow_post_types']); $post_types="'".$post_types."'";
+		$result=$wpdb->get_results("select * from ".$wpdb->prefix."posts where post_status = 'future' AND post_type IN(".$post_types.") ORDER BY post_date ASC ");
+
+	if(is_array($result))
+	{
+		echo '<div class="scheduled_posts_box">';
+		foreach($result as $scpost)
+		{
+			echo '<div class="scheduled_post"><div>'.get_date_from_gmt($scpost->post_date_gmt, $format = 'Y-m-d H:i:s')." | ".$scpost->post_title.'</div></div>';
+			//echo $scpost->post_title;
+		}
+		echo '</div>';
+	}
+	
+}#end wp_scheduled_posts()
+?>
